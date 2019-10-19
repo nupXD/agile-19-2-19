402a87b80809392d34778914cc08d941bc14f34f
NotTheAniket <aniketkhareldev@gmail.com>
2019-10-19 16:36:41 +0545

    added static pages

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://aniketkharel:' + encodeURIComponent('aniket123') + '@agile-19-2-19-kmsen.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
