ad54e70e7c7d7f00caf62e073fb1aebc78f77e37
NotTheAniket <aniketkhareldev@gmail.com>
2019-10-20 05:04:08 +0545

    added static pages routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const {
+  ensureAuthenticated,
+  forwardAuthenticated
+} = require('../config/auth');
 
-// Welcome Page
-router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+// Welcome page route for admins 
+// router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+
+// Welcome page route for normal users 
+router.get('/', (req, res) => res.render('landing'));
+
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
