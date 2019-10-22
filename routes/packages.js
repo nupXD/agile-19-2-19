9aeb904cd0e3b2ffc30c643e9a303f78e52a9923
NotTheAniket <aniketkhareldev@gmail.com>
2019-10-22 07:22:21 +0545

    added packages view and edit route

--- routes/packages.js
+++ routes/packages.js
@@ -7,6 +7,22 @@ const Packages = require('../models/packages.js');
 //get packages route
 router.get('/add', (req, res) => res.render('addPackages'));
 
+
+//view packages route
+router.get('/view', (req, res) => {
+    res.write("View all Packages Routes");
+});
+
+
+// edit packages route / get details about a package
+router.put('/edit{id}', (req, res) => {
+
+
+
+});
+
+
+
 //add packages routes
 router.post('/add', (req, res) => {
     const {
@@ -51,12 +67,5 @@ router.post('/add', (req, res) => {
 
 //end of add packages routes
 
-
-//edit packages route
-// router.get('/edit{id}', (requ, respo) => {
-
-
-
-// });
 
 module.exports = router;
